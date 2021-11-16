a9481abe271780cadce07118ab09288162effab3
mikima <mikimauri@gmail.com>
2021-11-16 10:18:21 +0100

    added p5 project

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 console.log("hello world!");
+
+// load express
+let express = require("express");
+
+// create an app
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+console.log("running server on http://localhost:" + port);
+
+app.use(express.static("public"));
